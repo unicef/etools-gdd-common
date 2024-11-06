34b909322cf09d7659d5bcb81c1370e23759801e
Dan <dan.cioara@nordlogic.com>
2024-11-06 15:19:48 +0200

    if in amendment, hide tabs: monitoring activities, results, reports

--- intervention-tabs.ts
+++ intervention-tabs.ts
@@ -237,6 +237,7 @@ export class GDDInterventionTabs extends connectStore(UploadMixin(LitElement)) {
                 slot="nav"
                 panel="${t.tab}"
                 ?active="${this.activeTab === t.tab}"
+                ?hidden="${t.hidden}"
                 >${t.tabLabel}</sl-tab
               >`
           )}
@@ -588,7 +589,6 @@ export class GDDInterventionTabs extends connectStore(UploadMixin(LitElement)) {
     if (!progressTabs) {
       this.pageTabs.push(...cloneDeep(this.progressTabTemplate));
     }
-
     if (envFlags && !envFlags.prp_mode_off && !this.pageTabs?.find((t: any) => t.tab === GDD_TABS.ResultsReported)) {
       // @ts-ignore
       this.pageTabs.push(
@@ -601,7 +601,12 @@ export class GDDInterventionTabs extends connectStore(UploadMixin(LitElement)) {
         {tabLabel: translate('REPORTS'), tabLabelKey: 'REPORTS', tab: GDD_TABS.Reports, hidden: false}
       );
     }
-
+    const tabsToHideInAmendment = [GDD_TABS.MonitoringActivities, GDD_TABS.ResultsReported, GDD_TABS.Reports];
+    this.pageTabs.forEach((item) => {
+      if (tabsToHideInAmendment.includes(item.tab)) {
+        item.hidden = this.isInAmendment;
+      }
+    });
     // this.toggleSubtabs(progressTab, envFlags);
   }
 
